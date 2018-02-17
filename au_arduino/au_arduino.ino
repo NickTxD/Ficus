13656d92a82f0a1c14efc36481292cc148fc824a
NickTxD <akulinnikolajj@gmail.com>
2018-02-17 18:27:20 +0300

    Interrupt was added
    Added external interrupt for software reset of the device

--- au_arduino/au_arduino.ino
+++ au_arduino/au_arduino.ino
@@ -6,15 +6,16 @@
 DHT dht(DHTPIN, DHTTYPE);       // Initialize the sensor
 
 int errors;                     // Number of consecutive errors reading from the sensor
+volatile int state = LOW;
 
 struct sensors_struct {
   int temperature;
   int humidity;
   int hic;
-//int brightness;
+  //int brightness;
   int soil;
   char msg[20];
-}sensors;
+} sensors;
 
 
 void setup() {
@@ -27,21 +28,26 @@ void setup() {
   errors = 0;                    // reset the counter
 
   Serial.println("Start");
+
+
+  pinMode(2,INPUT_PULLUP);
+  attachInterrupt(0, interrupt, FALLING);
 }
 
 void(* reset) (void) = 0;
-
+void interrupt(){
+  reset();
+}
 void loop() {
-  
   delay(2000);
-  
- if(errors>4)
+
+  if (errors > 4)
     reset();
-    
+
   getDataFromSensors();
 }
 
-void printDebug(){
+void printDebug() {
   Serial.print("Humidity: ");
   Serial.print(sensors.humidity);
   Serial.print(" % ");
@@ -62,34 +68,34 @@ void getDataFromSensors() {
   float humidity = dht.readHumidity();        //Read humidity
   float temperature = dht.readTemperature();  // Read temperature as Celsius
   float soil = analogRead(SOILPIN);           // Read soil dryness
-  
+
   if (isnan(humidity) || isnan(temperature)) {       // Check if any reads failed and exit early (to try again).
     Serial.println("Failed to read from DHT sensor!");
     errors++; //increase the counter
     return;
   }
   errors = 0; //reset the counter
-  
+
   float hic = dht.computeHeatIndex(temperature, humidity, false); // Compute heat index in Celsius (isFahreheit = false)
 
   //save data to struct
   sensors.temperature = temperature;
   sensors.humidity = humidity;
   sensors.hic = (int)hic;
-  sensors.soil = (int)(soil/10.23);
+  sensors.soil = (int)(soil / 10.23);
   sprintf(sensors.msg, "t%dh%di%ds%d", sensors.temperature, sensors.humidity, sensors.hic, sensors.soil);
 
+  printDebug();
 }
 
 // function that executes whenever data is received from master
-/*void receiveEvent(int howMany) {
-  while (0 < Wire.available()) {
-    char c = Wire.read();      
-    Serial.print(c);          
-  }
-  Serial.println();             
-}*/
-
+void receiveEvent(int howMany) {
+  /*while (0 < Wire.available()) {
+    char c = Wire.read();
+    Serial.print(c);
+    }
+    Serial.println();    */
+}
 // function that executes whenever data is requested from master
 void requestEvent() {
   Wire.write(sensors.msg);  /*send string on request */
