f152d9780604f46a31c601e6c67834ebb77b6524
NickTxD <akulinnikolajj@gmail.com>
2018-03-28 17:44:16 +0300

    reduced serial messages

--- au_esp_lamp/au_esp_lamp.ino
+++ au_esp_lamp/au_esp_lamp.ino
@@ -69,13 +69,6 @@ void setup_wifi() {
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
   if((char)payload[0] == '0')
     Serial.write(codeOff, 4);
   else
